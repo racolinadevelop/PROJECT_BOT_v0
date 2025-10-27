2e49510a24403922c252298c8661904163c517dd
Ramon <racolinadevelop@gmail.com>
2025-10-26 23:33:44 -0400

    fix 2

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"toaHLpTv7PDocXv1AT1Fusx64eeaHYsWB1TZrcTiONg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O23PESm2pEhyCAOBrLlCzel2MJbr3IOlB5Ga4Lpu4aY=\"\n}"
